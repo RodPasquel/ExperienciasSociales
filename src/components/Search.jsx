a320baa7939f26c07882669e046a86bf92e6f5ce
Carlos Perez <carlos@ioncom.com.mx>
2019-11-07 16:21:50 -0600

    ajustes de css para version movil del home

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 /* import CalendarRank from './CalendarRank'; */
 import Calendar from './Calendar';
@@ -12,19 +13,19 @@ const Search = () => {
         <h1 className='search__title'>Busca y reserva alojamiento</h1>
         <form action='' className='search__form'>
           <div>
-            <label htmlFor=''>Dónde</label>
-            <input id='' className='input' type='text' placeholder='México' />
+            <label htmlFor='inputLocation'> Dónde </label>
+            <input id='inputLocation' className='input' type='text' placeholder='México' />
           </div>
           <div>
-            <label htmlFor=''>Fecha de llegada y salida</label>
+            <label htmlFor='divCalendar'> Fecha de llegada y salida </label>
             {/* <CalendarRank
               disableDates={date => date < new Date().getTime()}
             /> */}
             <Calendar />
           </div>
           <div>
-            <label htmlFor=''>Huéspedes</label>
-            <input id='' className='input' type='number' min='1' placeholder='1' />
+            <label htmlFor='inputPeople'> Personas que asistiran</label>
+            <input id='inputPeople' className='input' type='number' min='1' placeholder='1' />
           </div>
           <div>
             <button className='button' type='button'>Buscar</button>
