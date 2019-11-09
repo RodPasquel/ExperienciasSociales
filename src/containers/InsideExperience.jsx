d698d07dba8af7a6fc3be20d62d53422d251a5de
Carlos Perez <carlos.perez@charliegperez.com>
2019-11-09 00:31:25 -0600

    creacion de la rama InteriorExperiencia y del contenedor InsideExperience.jsx

---
+++ src/containers/InsideExperience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InsideExperience = () => {
+  return (
+    <div>
+      <h2> Vivie la experiencia de </h2>
+    </div>
+  );
+};
+
+export default InsideExperience;
+
